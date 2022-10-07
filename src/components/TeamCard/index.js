1a0bfe7bb7fdcce6184cb051b4aba9cad85124e6
Surya T N <suryatej.nim@yahoo.com>
2022-10-07 07:41:40 +0000

    Implemented JSX elements, with Link component and standard elements to render intended UI with TeamCard component

--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -1,1 +1,18 @@
-// Write your code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const TeamCard = props => {
+  const {cardData} = props
+  const {id, name, teamImageUrl} = cardData
+
+  return (
+    <Link to={`/team-matches/${id}`}>
+      <li className="team-card-container">
+        <img className="team-image" src={teamImageUrl} alt={name} />
+        <h1 className="team-name">{name}</h1>
+      </li>
+    </Link>
+  )
+}
+
+export default TeamCard
