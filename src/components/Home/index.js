fbf58363eb68e2b96bce34348bcace9ca0941e96
Surya T N <suryatej.nim@yahoo.com>
2022-10-07 07:20:30 +0000

    Updated CSS styling for IPL logo image across view-port widths. Implemented JSX elements to render the intended UI with Home component.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,56 @@
-// Write your code here
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import TeamCard from '../TeamCard'
+
+import './index.css'
+
+const teamsApiUrl = 'https://apis.ccbp.in/ipl'
+
+export default class Home extends Component {
+  state = {
+    isLoading: true,
+    teamsData: [],
+  }
+
+  componentDidMount() {
+    this.fetchTeamsData()
+  }
+
+  fetchTeamsData = async () => {
+    const teamsDataAPIResponse = await fetch(teamsApiUrl)
+    const fetchedTeamsData = await teamsDataAPIResponse.json()
+    const formattedTeamsData = fetchedTeamsData.map(fetchedTeamsDataItem => ({
+      id: fetchedTeamsDataItem.id,
+      name: fetchedTeamsDataItem.name,
+      teamImageUrl: fetchedTeamsDataItem.team_image_url,
+    }))
+
+    this.setState({
+      isLoading: false,
+      teamsData: formattedTeamsData,
+    })
+  }
+
+  render() {
+    const {isLoading, teamsData} = this.state
+
+    return isLoading ? (
+      //   <div testid="loader">
+      <div>
+        <Loader type="Oval" color="#ffffff" height={50} width={50} />
+      </div>
+    ) : (
+      <div className="ipl-dashboard-bg-container">
+        <img
+          className="ipl-logo-img"
+          src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
+          alt="ipl logo"
+        />
+        {teamsData.map(teamsDataItem => (
+          <TeamCard key={teamsDataItem.id} cardData={teamsDataItem} />
+        ))}
+      </div>
+    )
+  }
+}
