2a7db4a3b9570decdd658a775178af9f0c5ea489
Surya T N <suryatej.nim@yahoo.com>
2022-10-08 17:52:19 +0000

    Adjusted CSS styling of JSx elements in TeamCard component to render the intended UI, like setting container background color with opacity, its witdth, spacing, sizing and removing text decoration of text wrapped inside the Link component imported from react-router-dom package

--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -6,7 +6,7 @@ const TeamCard = props => {
   const {id, name, teamImageUrl} = cardData
 
   return (
-    <Link to={`/team-matches/${id}`}>
+    <Link className="team-card-routing-link" to={`/team-matches/${id}`}>
       <li className="team-card-container">
         <img className="team-image" src={teamImageUrl} alt={name} />
         <h1 className="team-name">{name}</h1>
