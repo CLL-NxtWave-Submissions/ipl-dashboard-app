66574a6b38daff6a0a2fff03c8ac8e72b1858c75
Surya T N <suryatej.nim@yahoo.com>
2022-10-08 08:22:03 +0000

    Formatted code written in LatestMatch component. Added CSS styling for JSX elements in MatchCard component across view-port widths.

--- src/components/LatestMatch/index.js
+++ src/components/LatestMatch/index.js
@@ -15,28 +15,32 @@ const LatestMatch = props => {
   } = matchData
 
   return (
-      <div className="latest-match-bg-container">
-          <div className="competing-team-data-and-match-summary-container">
-              <div className="latest-match-summary">
-                <h1 className="competing-team-name-header">{competingTeam}</h1>
-                <p className="match-date">{date}</p>
-                <p className="match-venue">{venue}</p>
-                <p className="match-result">{result}</p>
-              </div>
-              <img className="competing-team-logo" src={competingTeamLogo} alt={competingTeam} />
-          </div>
-
-          <div className="latest-match-participants-and-rewards">
-            <p className="innings-header">First Innings</p>
-            <p className="innings-team-name">{firstInnings}</p>
-            <p className="innings-header">Second Innings</p>
-            <p className="innings-team-name">{secondInnings}</p>
-            <p className="man-of-the-match-header">Man Of The Match</p>
-            <p className="man-of-the-match">{manOfTheMatch}</p>
-            <p className="umpires-header">Umpires</p>
-            <p className="umpires">{umpires}</p>
-          </div>
+    <div className="latest-match-bg-container">
+      <div className="competing-team-data-and-match-summary-container">
+        <div className="latest-match-summary">
+          <h1 className="competing-team-name-header">{competingTeam}</h1>
+          <p className="match-date">{date}</p>
+          <p className="match-venue">{venue}</p>
+          <p className="match-result">{result}</p>
+        </div>
+        <img
+          className="competing-team-logo"
+          src={competingTeamLogo}
+          alt={competingTeam}
+        />
       </div>
+
+      <div className="latest-match-participants-and-rewards">
+        <p className="innings-header">First Innings</p>
+        <p className="innings-team-name">{firstInnings}</p>
+        <p className="innings-header">Second Innings</p>
+        <p className="innings-team-name">{secondInnings}</p>
+        <p className="man-of-the-match-header">Man Of The Match</p>
+        <p className="man-of-the-match">{manOfTheMatch}</p>
+        <p className="umpires-header">Umpires</p>
+        <p className="umpires">{umpires}</p>
+      </div>
+    </div>
   )
 }
 
