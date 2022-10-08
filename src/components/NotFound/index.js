2a29b45f443f110052936592d27059dd0d995964
Surya T N <suryatej.nim@yahoo.com>
2022-10-08 10:04:25 +0000

    Implemented JSX elements to render intended UI with NotFound component

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,1 +1,9 @@
-// Write your code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-bg-container">
+    <h1 className="not-found-message">Page Not Found !!</h1>
+  </div>
+)
+
+export default NotFound
