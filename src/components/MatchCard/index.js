028dc86f96fead87ecc7f5820078d4ca597039ca
Surya T N <suryatej.nim@yahoo.com>
2022-10-08 09:46:03 +0000

    Implemented JSX elements in MatchCard to render the intended UI

--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -1,1 +1,21 @@
-// Write your code here
+import './index.css'
+
+const MatchCard = props => {
+  const {matchCardData} = props
+  const {competingTeamLogo, competingTeam, result, matchStatus} = matchCardData
+
+  return (
+    <li className="match-card-bg-container">
+      <img
+        className="match-card-competing-team-logo"
+        src={competingTeamLogo}
+        alt={competingTeam}
+      />
+      <p className="match-card-competing-team-name">{competingTeam}</p>
+      <p className="match-card-result">{result}</p>
+      <p className="match-card-status">{matchStatus}</p>
+    </li>
+  )
+}
+
+export default MatchCard
